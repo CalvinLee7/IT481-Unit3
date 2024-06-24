c8e91336da393849ff04e851274dbc72a88e489d
callee <callee@CALLEE01>
2024-06-24 13:13:17 -0400

    Add project files.

---
+++ Unit3Winform/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace Unit2Winform
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonInitDBConn = new System.Windows.Forms.Button();
+            this.buttonCNumber = new System.Windows.Forms.Button();
+            this.buttonCName = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonInitDBConn
+            // 
+            this.buttonInitDBConn.Location = new System.Drawing.Point(35, 35);
+            this.buttonInitDBConn.Name = "buttonInitDBConn";
+            this.buttonInitDBConn.Size = new System.Drawing.Size(154, 23);
+            this.buttonInitDBConn.TabIndex = 0;
+            this.buttonInitDBConn.Text = "Initialize DB Connection";
+            this.buttonInitDBConn.UseVisualStyleBackColor = true;
+            this.buttonInitDBConn.Click += new System.EventHandler(this.buttonInitDBConn_Click);
+            // 
+            // buttonCNumber
+            // 
+            this.buttonCNumber.Location = new System.Drawing.Point(35, 76);
+            this.buttonCNumber.Name = "buttonCNumber";
+            this.buttonCNumber.Size = new System.Drawing.Size(154, 23);
+            this.buttonCNumber.TabIndex = 1;
+            this.buttonCNumber.Text = "Customer Number";
+            this.buttonCNumber.UseVisualStyleBackColor = true;
+            this.buttonCNumber.Click += new System.EventHandler(this.buttonCNumber_Click);
+            // 
+            // buttonCName
+            // 
+            this.buttonCName.Location = new System.Drawing.Point(35, 117);
+            this.buttonCName.Name = "buttonCName";
+            this.buttonCName.Size = new System.Drawing.Size(154, 23);
+            this.buttonCName.TabIndex = 2;
+            this.buttonCName.Text = "Customer Name";
+            this.buttonCName.UseVisualStyleBackColor = true;
+            this.buttonCName.Click += new System.EventHandler(this.buttonCName_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(114, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(8, 9);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(222, 195);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.buttonCName);
+            this.Controls.Add(this.buttonCNumber);
+            this.Controls.Add(this.buttonInitDBConn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonInitDBConn;
+        private System.Windows.Forms.Button buttonCNumber;
+        private System.Windows.Forms.Button buttonCName;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Unit3Winform/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Unit2Winform
+{
+    class DB
+    {
+        string dbConnecionString;
+        SqlConnection dbConnection;
+
+        public DB(string conn)
+        {
+            dbConnecionString = conn;
+        }
+
+        public string getCustomerCount()
+        {
+            Int32 cCount = 0;
+
+            dbConnection = new SqlConnection(dbConnecionString);
+            dbConnection.Open();
+            string countQuery = "select count(*) from customers;";
+            SqlCommand sqlcmd = new SqlCommand(countQuery, dbConnection);
+
+            try
+            {
+                cCount = Convert.ToInt32(sqlcmd.ExecuteScalar());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            return cCount.ToString();
+
+        }
+
+        public string getCustomerName()
+        {
+            string cNames = string.Empty;
+            SqlDataReader dataReader;
+
+            dbConnection = new SqlConnection(dbConnecionString);
+            dbConnection.Open();
+            string cNameQuery = "select companyname from customers;";
+            SqlCommand sqlcmd = new SqlCommand(cNameQuery, dbConnection);
+            dataReader = sqlcmd.ExecuteReader(); ;
+            while (dataReader.Read())
+            {
+                try
+                {
+                    cNames = $"{cNames}{dataReader.GetValue(0)} \n ";
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            return cNames;
+
+        }
+
+    }
+    public partial class Form1 : Form
+    {
+
+        DB database;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+
+        private void buttonInitDBConn_Click(object sender, EventArgs e)
+        {
+            database = new DB($@"server = CALLEE01\SQLEXPRESS;Trusted_Connection=true;Database=Northwind;User Instance=false;Connection Timeout=30");
+
+            MessageBox.Show("Database connection initialized");
+        }
+
+        private void buttonCNumber_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Please initailze the database connection");
+            }
+            else
+            {
+                try
+                {
+                    string cCount = database.getCustomerCount();
+                    MessageBox.Show(cCount, "Customer Count");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+
+        private void buttonCName_Click(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Please initailze the database connection");
+            }
+            else
+            {
+                try
+                {
+                    string cNames = database.getCustomerName();
+                    MessageBox.Show(cNames, "Customer Name");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
